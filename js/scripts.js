b59d64f514ef8220f7ab17a0a16b85e7435166d5
smokeyice <mwangi.ian3@gmail.com>
2018-08-19 09:25:47 +0300

    Marks Evaluation Loop Created

--- js/scripts.js
+++ js/scripts.js
@@ -5,59 +5,31 @@ $(document).ready(function(){
 });
 
 function checkSubmit(){
-  
+
+  var que1 = document.quiz.q1AnswerChoice.value;
+  var que2 = document.quiz.q2AnswerChoice.value;
+  var que3 = document.quiz.q3AnswerChoice.value;
+  var que4 = document.quiz.q4AnswerChoice.value;
+
+  var right = 0;
+
+  if (que1 == "a"){
+    right ++;
+  }
+
+  if (que2 == "client"){
+    right++
+  }
+
+  if (que3 == "addInteractivity") {
+    right++;
+  }
+
+
+  if (que4 == "interpreted") {
+    right++;
+  }
+
+  document.getElementById("afterSubmit").style.visibility="visible";
+  document.getElementById("number-correct").innerHTML = "You got " +right + " right!";
 }
-
-function evaluate(){
-  var que1 = document.quiz.scripting.value;
-  var que2 = document.quiz.client.value;
-  var que3 = document.quiz.styleHtml.value;
-  var que4 = document.quiz.compiled.value;
-
-  var correctAnswer =0;
-
-  if (que1 === "A Scripting"){
-    correctAnswer +=10;
-  }
-
-  if (que2 === "A Scripting") {
-    correctAnswer += 10;
-  }
-
-  if (que3 === "A Scripting") {
-    correctAnswer += 10;
-  }
-
-  if (que4 === "A Scripting") {
-    correctAnswer3 += 10;
-  }
-
-
-
-
-  var rewardMessage = ['Excellent!', 'Good!', 'Fair', 'Pull up your socks!'];
-
-
-
-
-var range;
-
-if (correct <= 1) {
-  range = 3;
-} else if (correct < 2) {
-  range = 2;
-} else if (correct < 3) {
-  range = 1;
-} else if (correct < 4) {
-  range = 0;
-}
-
-
-
-
-
-}
-
-
-//function showResults()
-//function buildQuiz(){}
